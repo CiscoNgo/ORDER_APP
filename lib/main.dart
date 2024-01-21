baded8f370179ab26d700e07ed12fe83f3aa1627
phat dat <datb2014735@student.ctu.edu.vn>
2024-01-21 14:28:55 +0700

    doi theme cho ung dung

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quan ly nha hang',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.purple,
+        ).copyWith(
+          secondary: Colors.deepOrange,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('MyShop'),
+        ),
+        body: const Center(child: Text('welcom to nha hang'),
+          ),
+        ),
+    );
+  }
+}
